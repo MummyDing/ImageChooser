8f86b8585543fcf9bc530ac0bbdeb4cdc29ee6a4
MummyDing <mummydingz@gmail.com>
2015-10-24 11:47:07 +0800

    完成基本图片选择功能

--- app/src/main/java/com/demo/mummyding/imagechooser/MainActivity.java
+++ app/src/main/java/com/demo/mummyding/imagechooser/MainActivity.java
@@ -1,7 +1,9 @@
 package com.demo.mummyding.imagechooser;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -13,94 +15,79 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String IMAGE_FILE_NAME = "avatarImage.jpg";// 头像文件名称
-    private static final int REQUESTCODE_PICK = 0;		// 相册选图标记
-    private static final int REQUESTCODE_TAKE = 1;		// 相机拍照标记
-    private static final int REQUESTCODE_CUTTING = 2;	// 图片裁切标记
-    private ImageView imageView;
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener{
+    private final int PICK_PHOTO = 1;
+    private final int CAMERA_PHOTO=2;
+    private final int MAX_IMAGE = 3;
+    private LinearLayout imageSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.imageholder);
+        imageSet = (LinearLayout) findViewById(R.id.imageSet);
+
     }
     public void onPhoto(View v){
-        Toast.makeText(MainActivity.this,"OK",Toast.LENGTH_SHORT).show();
-        Intent pickIntent = new Intent(Intent.ACTION_PICK, null);
-        // 如果朋友们要限制上传到服务器的图片类型时可以直接写如："image/jpeg 、 image/png等的类型"
-        pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-        startActivityForResult(pickIntent, REQUESTCODE_PICK);
+        try{
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+            startActivityForResult(intent, PICK_PHOTO);
+        }catch (Exception e){
+
+        }
     }
     public void onCamera(View v){
-        Log.d("TAg", "camera");
-        Intent takeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //下面这句指定调用相机拍照后的照片存储的路径
-        takeIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                Uri.fromFile(new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME)));
-        startActivityForResult(takeIntent, REQUESTCODE_TAKE);
+        try{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, CAMERA_PHOTO);
+        }catch (Exception e){
+
+        }
     }
 
     @Override
-    public void onActivityResult(int requestCode,int resultCode, Intent data) {
-        switch (resultCode){
-            case REQUESTCODE_TAKE:
-                File temp = new File(Environment.getExternalStorageDirectory() + "/" + IMAGE_FILE_NAME);
-                startPhotoZoom(Uri.fromFile(temp));
-                break;
-            case REQUESTCODE_PICK:
-                try {
-                    startPhotoZoom(data.getData());
-                } catch (NullPointerException e) {
-                    e.printStackTrace();// 用户点击取消操作
-                }
-                break;
-            case REQUESTCODE_CUTTING:// 取得裁剪后的图片
-                if (data != null) {
-                    setPicToView(data);
-                  //  imageView.set
-                }
-                break;
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    /**
-     * 保存裁剪之后的图片数据
-     * @param picdata
-     */
-    private void setPicToView(Intent picdata) {
-        Bundle extras = picdata.getExtras();
-        if (extras != null) {
-            // 取得SDCard图片路径做显示
-            Bitmap photo = extras.getParcelable("data");
-            Drawable drawable = new BitmapDrawable(null, photo);
-            imageView.setImageDrawable(drawable);
+        switch (requestCode){
+            case PICK_PHOTO:
+                if(data == null) return;
+                if(imageSet.getChildCount() >= MAX_IMAGE){
+                    Toast.makeText(MainActivity.this,"最多只能添加"+MAX_IMAGE+"张图片",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Uri uri= data.getData();
+                ImageView imageView= new ImageView(this);
+                imageView.setImageURI(uri);
+                imageView.setLongClickable(true);
+                imageView.setOnLongClickListener(this);
+                imageSet.addView(imageView);
+                break;
+            case CAMERA_PHOTO:
+                Bitmap bitmap = data.getParcelableExtra("data");
+                if(bitmap == null) return;
+                if(imageSet.getChildCount() >= MAX_IMAGE){
+                    Toast.makeText(MainActivity.this,"最多只能添加"+MAX_IMAGE+"张图片",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                imageView= new ImageView(this);
+                imageView.setImageBitmap(bitmap);
+                imageView.setLongClickable(true);
+                imageView.setOnLongClickListener(this);
+                imageSet.addView(imageView);
+                break;
         }
     }
-
-    /**
-     * 裁剪图片方法实现
-     * @param uri
-     */
-    public void startPhotoZoom(Uri uri) {
-        Intent intent = new Intent("com.android.camera.action.CROP");
-        intent.setDataAndType(uri, "image/*");
-        // crop=true是设置在开启的Intent中设置显示的VIEW可裁剪
-        intent.putExtra("crop", "true");
-        // aspectX aspectY 是宽高的比例
-        intent.putExtra("aspectX", 1);
-        intent.putExtra("aspectY", 1);
-        // outputX outputY 是裁剪图片宽高
-        intent.putExtra("outputX", 300);
-        intent.putExtra("outputY", 300);
-        intent.putExtra("return-data", true);
-        startActivityForResult(intent, REQUESTCODE_CUTTING);
+    @Override
+    public boolean onLongClick(View v) {
+        imageSet.removeView(v);
+        return true;
     }
-
 }
